de753e0ffec0f79596056b8a0308693dbcc00f62
rh-abir <rhabir.co@gmail.com>
2024-01-30 19:09:48 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">This is react app</h1>
+    </div>
+  );
+};
+
+export default App;
