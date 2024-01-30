0d83959d2a76268ff8201344dc354fccfe010044
rh-abir <rhabir.co@gmail.com>
2024-01-30 21:57:22 +0600

    learn drag

--- src/page/Lesson1.tsx
+++ src/page/Lesson1.tsx
@@ -1,19 +1,20 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Lesson1 = () => {
   const parent = {
     hidden: { opacity: 0, scale: 0.9 },
     visible: {
-      opacity: 1,
+      opacity: 0.6,
       scale: 1,
       transition: {
         ease: "easeInOut",
-        duration: 2,
+        duration: 0.5,
       },
     },
 
-    hover: { scale: 1.1, transition: { duration: 1 } },
-    click: { scale: 1, rotate: 45 },
+    hover: { opacity: 1 },
+    tap: { scale: 1.1, boxShadow: "0px 10px 10px #000" },
   };
 
   //   const child = {
@@ -28,17 +29,23 @@ const Lesson1 = () => {
   //     },
   //   };
 
+  const prentRef = useRef(null);
+
   return (
-    <div>
+    <div
+      ref={prentRef}
+      className="border border-red-500 size-[500px] flex justify-center items-center"
+    >
       <motion.div
         className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
         variants={parent}
         initial="hidden"
         animate="visible"
         whileHover="hover"
-        whileTap="click"
-        onHoverStart={() => console.log("hover start")}
-        onHoverEnd={() => console.log("hover end")}
+        drag
+        dragElastic={0.7}
+        dragConstraints={prentRef}
+        whileDrag="tap"
       >
         {/* <motion.div
           variants={child}
